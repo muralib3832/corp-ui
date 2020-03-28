659a13f403bbcfcf2b769c5ac25402dcc259aa0e
muralikrishnab22mk <muralikrishnab22.mk@gmail.com>
2020-03-28 11:56:34 +0530

    intial commit'

---
+++ build/precache-manifest.572c6139eeee914f114c68a62efbb572.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e370de6b1bcd1a2206fe6f831178acf7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "18ce2596599a8de59aa9",
+    "url": "./static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "10ace95db84ec058b9c4",
+    "url": "./static/css/main.3f1224a5.chunk.css"
+  },
+  {
+    "revision": "18ce2596599a8de59aa9",
+    "url": "./static/js/2.3ff6bd62.chunk.js"
+  },
+  {
+    "revision": "4aa0fe1eaee3dcf45338a38641d40f08",
+    "url": "./static/js/2.3ff6bd62.chunk.js.LICENSE"
+  },
+  {
+    "revision": "10ace95db84ec058b9c4",
+    "url": "./static/js/main.b35aed2b.chunk.js"
+  },
+  {
+    "revision": "58d6219fe1f60f2fa5af",
+    "url": "./static/js/runtime-main.f1c188a0.js"
+  }
+]);
